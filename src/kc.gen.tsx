7c476bbc3d020fc1b7b0ce674f8ec2b93b423f42
Yashdeep Tandon <Yashdeep.Tandon@appvintech.com>
2025-12-03 13:42:53 +0530

    fixes

--- src/kc.gen.tsx
+++ src/kc.gen.tsx
@@ -10,9 +10,9 @@
 
 import { lazy, Suspense, type ReactNode } from "react";
 
-export type ThemeName = "keycloakify-starter";
+export type ThemeName = "gotrust-theme";
 
-export const themeNames: ThemeName[] = ["keycloakify-starter"];
+export const themeNames: ThemeName[] = ["gotrust-theme"];
 
 export type KcEnvName = never;
 
